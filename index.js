bcbc1f6a7a1b0b6261e14400b126193a74500683
shakil <mdshakil2898@gmail.com>
2022-10-26 06:24:37 -0700

    courses router added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const catagories = require('./data/catagories.json');
+const courses = require('./data/courses.json')
 
 app.use(cors())
 
@@ -12,6 +13,16 @@ app.get('/', (req, res)=>{
 
 app.get('/catagories', (req, res) =>{
    res.send(catagories)
+})
+
+app.get('/courses', (req, res) =>{
+   res.send(courses)
+})
+
+app.get('/:id', (req, res)=>{
+   const id = (req.params.id)
+   const data = courses.find(course=> course.course_id == id);
+   res.send(data);
 })
 
 app.listen(port, ()=>{
